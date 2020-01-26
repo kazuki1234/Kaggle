a22d2e86d291ac4df2e2cd3cdba7d3b70ebc3aeb
若林一樹 <kazuki@wakabayashikazukinoMacBook-Pro.local>
2020-01-26 23:50:15 +0900

    null importance

---
+++ null_importance.py
@@ -0,0 +1,100 @@
+def get_feature_importances(data, shuffle, seed=None):
+    # Gather real features
+    train_features = [f for f in data if f not in ['TARGET', 'SK_ID_CURR']]
+    # Go over fold and keep track of CV score (train and valid) and feature importances
+    
+    # Shuffle target if required
+    y = data['TARGET'].copy()
+    if shuffle:
+        # Here you could as well use a binomial distribution
+        y = data['TARGET'].copy().sample(frac=1.0)
+    
+    # Fit LightGBM in RF mode, yes it's quicker than sklearn RandomForest
+    dtrain = lgb.Dataset(data[train_features], y, free_raw_data=False, silent=True)
+    lgb_params = {
+        'objective': 'binary',
+        'boosting_type': 'rf',
+        'subsample': 0.623,
+        'colsample_bytree': 0.7,
+        'num_leaves': 127,
+        'max_depth': 8,
+        'seed': seed,
+        'bagging_freq': 1,
+        'n_jobs': 4
+    }
+    
+    # Fit the model
+    clf = lgb.train(params=lgb_params, train_set=dtrain, num_boost_round=200, categorical_feature=categorical_feats)
+
+    # Get feature importances
+    imp_df = pd.DataFrame()
+    imp_df["feature"] = list(train_features)
+    imp_df["importance_gain"] = clf.feature_importance(importance_type='gain')
+    imp_df["importance_split"] = clf.feature_importance(importance_type='split')
+    imp_df['trn_score'] = roc_auc_score(y, clf.predict(data[train_features]))
+    
+    return imp_df
+
+# Seed the unexpected randomness of this world
+np.random.seed(123)
+# Get the actual importance, i.e. without shuffling
+actual_imp_df = get_feature_importances(data=data, shuffle=False)
+
+null_imp_df = pd.DataFrame()
+nb_runs = 80
+import time
+start = time.time()
+dsp = ''
+for i in range(nb_runs):
+    # Get current run importances
+    imp_df = get_feature_importances(data=data, shuffle=True)
+    imp_df['run'] = i + 1 
+    # Concat the latest importances with the old ones
+    null_imp_df = pd.concat([null_imp_df, imp_df], axis=0)
+    # Erase previous message
+    for l in range(len(dsp)):
+        print('\b', end='', flush=True)
+    # Display current run and time used
+    spent = (time.time() - start) / 60
+    dsp = 'Done with %4d of %4d (Spent %5.1f min)' % (i + 1, nb_runs, spent)
+    print(dsp, end='', flush=True)
+
+def display_distributions(actual_imp_df_, null_imp_df_, feature_):
+    plt.figure(figsize=(13, 6))
+    gs = gridspec.GridSpec(1, 2)
+    # Plot Split importances
+    ax = plt.subplot(gs[0, 0])
+    a = ax.hist(null_imp_df_.loc[null_imp_df_['feature'] == feature_, 'importance_split'].values, label='Null importances')
+    ax.vlines(x=actual_imp_df_.loc[actual_imp_df_['feature'] == feature_, 'importance_split'].mean(), 
+               ymin=0, ymax=np.max(a[0]), color='r',linewidth=10, label='Real Target')
+    ax.legend()
+    ax.set_title('Split Importance of %s' % feature_.upper(), fontweight='bold')
+    plt.xlabel('Null Importance (split) Distribution for %s ' % feature_.upper())
+    # Plot Gain importances
+    ax = plt.subplot(gs[0, 1])
+    a = ax.hist(null_imp_df_.loc[null_imp_df_['feature'] == feature_, 'importance_gain'].values, label='Null importances')
+    ax.vlines(x=actual_imp_df_.loc[actual_imp_df_['feature'] == feature_, 'importance_gain'].mean(), 
+               ymin=0, ymax=np.max(a[0]), color='r',linewidth=10, label='Real Target')
+    ax.legend()
+    ax.set_title('Gain Importance of %s' % feature_.upper(), fontweight='bold')
+    plt.xlabel('Null Importance (gain) Distribution for %s ' % feature_.upper())
+
+display_distributions(actual_imp_df_=actual_imp_df, null_imp_df_=null_imp_df, feature_='LIVINGAPARTMENTS_AVG')
+
+display_distributions(actual_imp_df_=actual_imp_df, null_imp_df_=null_imp_df, feature_='CODE_GENDER')
+
+display_distributions(actual_imp_df_=actual_imp_df, null_imp_df_=null_imp_df, feature_='EXT_SOURCE_1')
+
+# 閾値を設定
+THRESHOLD = 80
+
+# 閾値を超える特徴量を取得
+imp_features = []
+for feature in actual_imp_df["feature"]:
+    actual_value = actual_imp_df.query(f"feature=='{feature}'")["importance"].values
+    null_value = null_imp_df.query(f"feature=='{feature}'")["importance"].values
+    percentage = (null_value < actual_value).sum() / null_value.size * 100
+    if percentage >= THRESHOLD:
+        imp_features.append(feature)
+
+imp_features
